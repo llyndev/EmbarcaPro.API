0a369a907f81f747d0d8ddeca0cbf49fcd44ef86
llyndev <weslley.alves3310@gmail.com>
2026-07-08 23:48:57 -0300

    refactor: convert Address to record with English properties

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,16 +1,35 @@
-﻿namespace EmbarcaPro.API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmbarcaPro.API.Models
 {
-    public class Address
+    [ComplexType]
+    public record Address
     {
         // TODO: futuramente solciitar somente CEP e Numero.
 
-        public string Logradouro { get; private set; }
-        public string Numero { get; private set; }
-        public string Complement { get; private set; }
-        public string Bairro { get; private set; }
-        public string Cidade { get; private set; }
-        public string Estado { get; private set; }
-        public string Cep { get; private set; }
+        public string Street { get; init; } // Logradouro
+        public string Number { get; init; } // Numero
+        public string Complement { get; init; } // Complemento
+        public string Neighborhood { get; init; } // Bairro
+        public string City { get; init; } // Cidade
+        public string State { get; init; } // Estado
+        public string ZipCode { get; init; } // CEP
+
+        protected Address()
+        {
+
+        }
+
+        public Address (string street, string number, string complement, string neighborhood, string city, string state, string zipCode)
+        {
+            Street = street.Trim();
+            Number = number.Trim();
+            Complement = complement.Trim();
+            Neighborhood = neighborhood.Trim();
+            City = city.Trim();
+            State = state.Trim();
+            ZipCode = zipCode.Trim();
+        }
 
     }
 }
