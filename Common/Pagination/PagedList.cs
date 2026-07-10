ec94a32789b7ac657f6dbf07bd2840610df81aad
llyndev <weslley.alves3310@gmail.com>
2026-07-10 00:13:01 -0300

    feat: add generic PagedList support

---
+++ Common/Pagination/PagedList.cs
@@ -0,0 +1,21 @@
+﻿namespace EmbarcaPro.API.Common.Pagination
+{
+    public record PagedList<T>
+    {
+        public IEnumerable<T> Items { get; init; }
+        public int TotalCount { get; init; }
+        public int CurrentPage { get; init; }
+        public int PageSize { get; init; }
+
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+        public PagedList(IEnumerable<T> items, int totalCount, int currentPage, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+        }
+
+    }
+}
