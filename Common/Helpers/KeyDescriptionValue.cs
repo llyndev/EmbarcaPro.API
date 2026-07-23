7c76f02d0b23890dc999fbf034f00e19a739bb0e
llyndev <weslley.alves3310@gmail.com>
2026-07-23 13:28:40 -0300

    feat: adicionada classe genérica KeyDescriptionValue para mapeamento de enums

---
+++ Common/Helpers/KeyDescriptionValue.cs
@@ -0,0 +1,15 @@
+﻿namespace EmbarcaPro.API.Common.Helpers
+{
+    public class KeyDescriptionValue<T> where T : Enum
+    {
+
+        public T Key { get; }
+        public string Value { get; }
+
+        public KeyDescriptionValue(T key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
