7170a9243aed43a41bb5ed3ba66a817d430eb72b
llyndev <weslley.alves3310@gmail.com>
2026-07-24 13:53:42 -0300

    feat: adicionado classes CtePartner e Partner ao modelo CT-e

---
+++ Models/CtePartner.cs
@@ -0,0 +1,31 @@
+﻿using EmbarcaPro.API.Enums;
+
+namespace EmbarcaPro.API.Models
+{
+    /// <summary>
+    /// Entidade de associação pois o mesmo partner pode ser referenciados em papeis diferentes
+    /// (rem, dest, exped, receb) 
+    /// </summary>
+    public class CtePartner
+    {
+
+        public Guid Id { get; init; }
+        public Guid CteId { get; init; }
+        public Guid PartnerId { get; init; }
+        public Partner Partner { get; init; }
+        public PartnerType Type { get; init; }
+
+        protected CtePartner()
+        {
+
+        }
+
+        public CtePartner(Guid cteId, Partner partner, PartnerType type)
+        {
+            CteId = cteId;
+            Partner = partner;
+            PartnerId = partner.Id;
+            Type = type;
+        }
+    }
+}
---
+++ Models/Partner.cs
@@ -0,0 +1,31 @@
+﻿namespace EmbarcaPro.API.Models
+{
+    /// <summary>
+    /// Representa qualquer das 4 roles que aparecem no CT-e
+    /// (rem, dest, exped, receb)
+    /// </summary>
+    public class Partner
+    {
+
+        public Guid Id { get; init; }
+
+        public string CnpjOrCpf { get; init; } // CNPJ ou CPF
+        public string StateTaxId { get; init; } // IE - Inscrição Estadual
+        public string LegalNameOrFullName { get; init; } // Razão social ou nome
+        public Address Address { get; init; }
+        public string Phone { get; init; }
+        public string Email { get; init; }
+
+        protected Partner() {
+
+        }
+
+        public Partner(string cnpjOrCpf, string legalNameOrFullName, Address address, string stateTaxId)
+        {
+            CnpjOrCpf = cnpjOrCpf;
+            LegalNameOrFullName = legalNameOrFullName.Trim();
+            Address = address;
+            StateTaxId = stateTaxId.Trim();
+        }
+    }
+}
