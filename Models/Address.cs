09ca3a53a914e5b2a95ed02660d756b43b71dd4f
llyndev <weslley.alves3310@gmail.com>
2026-07-23 23:25:00 -0300

    feat: Atualizada estrutura de Address com dados fiscais e de contato

--- Models/Address.cs
+++ Models/Address.cs
@@ -2,18 +2,28 @@
 
 namespace EmbarcaPro.API.Models
 {
+    /// <summary>
+    /// Estrutura de endereço padronizada para comerciais e rotas de transportes.
+    /// </summary>
+    /// <remarks>
+    /// TODO: Integrar com a API do ViaCEP para autofill e validação de dados com base no CEP.
+    /// </remarks>
     [ComplexType]
     public record Address
     {
-        // TODO: futuramente solciitar somente CEP e Numero.
 
         public string Street { get; init; } // Logradouro
         public string Number { get; init; } // Numero
         public string Complement { get; init; } // Complemento
         public string Neighborhood { get; init; } // Bairro
+        public string IbgeCode { get; init; } // Tabela IBGE
         public string City { get; init; } // Cidade
+        public string Uf { get; init; } // UF estado
         public string State { get; init; } // Estado
         public string ZipCode { get; init; } // CEP
+        public string CountryCode { get; init; } = "1058"; // Brasil, padrão
+        public string Country { get; init; } = "Brasil";
+        public string Phone { get; init; }
 
         protected Address()
         {
