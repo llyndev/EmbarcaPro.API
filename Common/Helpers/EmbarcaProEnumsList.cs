78da831bc9889cdba9d46526fa85208e72b19609
llyndev <weslley.alves3310@gmail.com>
2026-07-30 23:08:33 -0300

    feat: adiciona método de extensão EnumResponse

--- Common/Helpers/EmbarcaProEnumsList.cs
+++ Common/Helpers/EmbarcaProEnumsList.cs
@@ -111,7 +111,7 @@ namespace EmbarcaPro.API.Common.Helpers
             {
                 new KeyDescriptionValue<CteStatus>(CteStatus.Draft, "R"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Authorized, "A"),
-                new KeyDescriptionValue<CteStatus>(CteStatus.Cancelled, "C"),
+                new KeyDescriptionValue<CteStatus>(CteStatus.Canceled, "C"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Denied, "D"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.AwaitingAuthorization, "W"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Rejected, "R"),
