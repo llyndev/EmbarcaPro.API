78b21ab9a68252c6fb7d5e32dba01ef24df3a7a7
llyndev <weslley.alves3310@gmail.com>
2026-07-31 18:50:44 -0300

    fix

--- Common/Helpers/EmbarcaProEnumsList.cs
+++ Common/Helpers/EmbarcaProEnumsList.cs
@@ -109,10 +109,10 @@ namespace EmbarcaPro.API.Common.Helpers
         {
             return new List<KeyDescriptionValue<CteStatus>>
             {
-                new KeyDescriptionValue<CteStatus>(CteStatus.Draft, "R"),
+                new KeyDescriptionValue<CteStatus>(CteStatus.Draft, "D"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Authorized, "A"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Canceled, "C"),
-                new KeyDescriptionValue<CteStatus>(CteStatus.Denied, "D"),
+                new KeyDescriptionValue<CteStatus>(CteStatus.Denied, "N"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.AwaitingAuthorization, "W"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Rejected, "R"),
                 new KeyDescriptionValue<CteStatus>(CteStatus.Contingency, "T")
